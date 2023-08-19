42cae7b4cd76919c07ade97b0701631a360d030e
NGUYENTRAN2806 <136039115+NGUYENTRAN2806@users.noreply.github.com>
2023-08-19 17:07:26 +0700

    Add files via upload

--- js/control.js
+++ js/control.js
@@ -1,9 +1,9 @@
-
-var typed = new Typed('.element', {	
-	strings: ["Web Designer.", "Code Developer.","Apps Designer."],
-    typeSpeed:50,
-    backSpeed:50,
-    loop:true,
-  });
-$('.counter').countUp();
 // JavaScript Document
+$('#cn').owlCarousel({
+   loop:true,	// cho lap lại
+	items:1,	// xuất hiện 1 ảnh thôi
+	smartSpeed:1000,   	// tốc độ thay đổi ảnh
+	autoplay:true,		// cho phép tự động chạy
+   autoplayTimeout:5000,  // thời gian chờ khi chuyển ảnh khi chạy tự động
+nav:true,	// cho xuất hiện bộ nút tới lui 
+ })
